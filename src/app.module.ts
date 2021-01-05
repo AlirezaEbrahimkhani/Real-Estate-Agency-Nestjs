ae63bd65ee7994e4af1552c2468ee71d1f9ef435
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-01-06 01:23:56 +0330

    Connect to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HouseModule } from './house/house.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://hamidreza:hamidreza123@home.lhb4w.mongodb.net/Home?retryWrites=true&w=majority',
     ),
+    HouseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
